0256ffcfaaad4144d804199e815e3e0a85d342ee
vbhagia <94276478+vbhagia@users.noreply.github.com>
2026-09-17 18:36:25 -0700

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # We can iterate through strs,
+        # and construct hashmap signatures for each string.
+        # We can then take those signatures, and match them in a hashmap
+        # The hashmap should have structure key: <alphabetized str>,
+        # val: <array of every instance of this anagram>
+
+        strmap = dict()
+
+        for s in strs:
+            sorted_s = sorted(s)
+            s_sorted = ""
+            for char in sorted_s:
+                s_sorted += char
+            
+            if s_sorted in strmap:
+                strmap[s_sorted].append(s)
+            else:
+                strmap[s_sorted] = [s]
+        
+        output = []
+        
+        for key, value in strmap.items():
+            output.append(value)
+        
+        return output
